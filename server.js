11a685531a3ded5bce391cb6d1fea3b197d8d750
HebaAlhamaydh <Eng_heba_hamaydh@hotmail.com>
2022-05-13 11:01:06 +0300

    done

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ const axios = require("axios").default;
 const recipesData = require("./Movie Data/data.json");
 
 const{Client}=require('pg');
-const client =new Client(url);
+// const client =new Client(url);
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+ });
 
 const app = express();
 app.use(cors());
